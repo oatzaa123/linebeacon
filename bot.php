4965e3766c0e4c0f3899a95f32bbec94a67ed209
oatzaa123 <53205207+oatzaa123@users.noreply.github.com>
2019-11-18 11:43:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,14 +18,14 @@ if ( sizeof($request_array['events']) > 0 ) {
     foreach ($request_array['events'] as $event) {
 
         $reply_message = '';
-        $reply_token = $event['replyToken'];
-
-        $text = $event['message']['text'];
-        $data = [
-            'replyToken' => $reply_token,
-            //'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
-            'messages' => [['type' => 'text', 'text' => $text ]]
-        ];
+      $reply_token = $event['replyToken'];
+      $data = [
+         'replyToken' => $reply_token,
+         'messages' => [
+            ['type' => 'text', 
+             'text' => json_encode($request_array)]
+         ]
+      ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
         $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
